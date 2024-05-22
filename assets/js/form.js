6105da3861ffd5a34ee09e6450b735f19ce0fff2
Bryson <brysob7617@gmail.com>
2024-05-22 12:02:56 -0400

    added a message when the from is left blank and submitted

--- assets/js/form.js
+++ assets/js/form.js
@@ -2,6 +2,12 @@ const usernameInput = document.querySelector('#username');
 const titleInput = document.querySelector('#title');
 const contentInput = document.querySelector('#content');
 const submitButton = document.querySelector('#submit');
+const msgDiv = document.querySelector('#msg');
+
+function displayMessage(type, message) {
+msgDiv.textContent = message;
+msgDiv.setAttribute('class', type);
+}
 
 submitButton.addEventListener('click', function (event) {
 event.preventDefault();
@@ -13,7 +19,28 @@ const userInput = {
     title: titleInput.value.trim(), 
     content: contentInput.value.trim(), 
 }
+
+const username = document.querySelector('#username').value
+const title = document.querySelector('#title').value
+const content = document.querySelector('#content').value
+
+if (username === ''){
+    displayMessage('error', 'Username cannot be blank');
+} else if ( title === '') {
+    displayMessage('error', 'Title cannot be blank');
+} else if (content === ''){
+    displayMessage('error', 'Content cannot be blank');
+} else {
+
+
+}
+
 userInputs.push(userInput);
 
 localStorage.setItem('userInputs', JSON.stringify(userInputs));
 });
+
+
+
+
+
